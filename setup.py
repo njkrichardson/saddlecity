c5472bf52e7cedb1ac172d8e091740ba10ae8963
Nick Richardson <nrichardson@unify.id>
2020-03-03 08:56:49 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(name='saddlecity',
+      version='0.1dev',
+      description='Nonlinear dynamical systems theory for models of games, biological memory, and switching autoregressive processes.',
+      author='Nick Richardson and Yoni Maltsman',
+      author_email='nrichardson@hmc.edu, ymaltsman@hmc.edu',
+      url='https://github.com/njkrichardson/saddlecity',
+      long_description=open('README.md').read(), 
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+      python_requires=">=3.6"
+     )
