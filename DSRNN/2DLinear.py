40ce003088bbc438fd007a4d5e7bd6142ec63c73
Yoni Maltsman <46268861+ymaltsman@users.noreply.github.com>
2020-04-23 17:10:45 -0500

    Fixed issues with previous RNN
    Changed the loss function. For some in the last iteration I was using a probability based loss function which doesn't make sense for imitating dynamical systems, so I switched to mean squared error. Also the RNN evaluates both dimensions of the DS

--- DSRNN/2DLinear.py
+++ DSRNN/2DLinear.py
@@ -6,6 +6,7 @@ import numpy as np
 from scipy.integrate import odeint
 import torch
 import torch.nn as nn
+import matplotlib.pyplot as plt
 
 def DE(s,t):
     """
@@ -29,21 +30,22 @@ import random
 
 def generate_data(batch_size,t_steps):
     """
-    returns a batch_size x t_steps matrix
+    returns a batch_size x t_steps x 2 data structure
     """
     #batch_size number of sequences, each w t_steps data points
-    data = np.zeros((batch_size, t_steps,1), dtype =np.float32)
+    data = np.zeros((batch_size, t_steps,2), dtype =np.float32)
     for i in range(batch_size):
         s0 = [random.uniform(-1,1),random.uniform(-1,1)]
         s=generatesamp(s0, t_steps)
         for j in range(t_steps):
             #data populated with x(t) for each t
-            data[i][j]=s[j][1]
+            data[i][j][0]=s[j][0]
+            data[i][j][1]=s[j][1]
     return data
 
 def input_and_target(data):
     """
-    Generates a matrix of input and target sequences
+    Generates a data structure of input sequences and one of target sequences
     """
     input_seq=[]
     target_seq=[]
@@ -57,15 +59,14 @@ batch_size=10
 t_steps=10
 data=generate_data(batch_size,t_steps)
 input_seq, target_seq=input_and_target(data)
-#print("input: ", input_seq, "target: ", target_seq)
-input_seq=np.array(input_seq)
-#print("Input shape: {} --> (Batch Size, Sequence Length, One-Hot Encoding Size)".format(input_seq.shape))
 
 
-#turn into tensors
-input_seq = torch.from_numpy(input_seq)
+
+#turn these structures into tensors
+input_seq = torch.Tensor(input_seq)
 target_seq = torch.Tensor(target_seq)
-print("input: ", len(input_seq[0]), "target: ", len(target_seq[0]))
+
+
 # torch.cuda.is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
 is_cuda = torch.cuda.is_available()
 
@@ -103,7 +104,7 @@ class Model(nn.Module):
         out, hidden = self.rnn(x, hidden)
         
         # Reshaping the outputs such that it can be fit into the fully connected layer
-        out = out.contiguous().view(-1, self.hidden_dim)
+        #out = out.contiguous().view(-1, self.hidden_dim)
         out = self.fc(out)
         
         return out, hidden
@@ -115,21 +116,22 @@ class Model(nn.Module):
         return hidden
 
 # Instantiate the model with hyperparameters
-model = Model(input_size=1, output_size=9, hidden_dim=10, n_layers=1)
+model = Model(input_size=2, output_size=2, hidden_dim=10, n_layers=1)
 # We'll also set the model to the device that we defined earlier (default is CPU)
 model = model.to(device)
 
-# Define hyperparameters
+# Define learning rate, # of training runs (epochs)
 n_epochs = 100
 lr=0.01
 
 # Define Loss, Optimizer
-criterion = nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
 
 # Training Run
 input_seq = input_seq.to(device)
+outputs=[]
 for epoch in range(1, n_epochs + 1):
     optimizer.zero_grad() # Clears existing gradients from previous epoch
     #input_seq = input_seq.to(device)
@@ -137,16 +139,27 @@ for epoch in range(1, n_epochs + 1):
 
     output = output.to(device)
     target_seq = target_seq.to(device)
-    #getting the error index error: target -1 out of bounds
-    #print("output:", output, "target:", target_seq)
-    loss = criterion(output, target_seq.view(-1).long())
+
+
+    loss = criterion(output, target_seq)
     loss.backward() # Does backpropagation and calculates gradients
     optimizer.step() # Updates the weights accordingly
     
     if epoch%10 == 0:
         print('Epoch: {}/{}.............'.format(epoch, n_epochs), end=' ')
         print("Loss: {:.4f}".format(loss.item()))
-        print("output:", output[0], "target:", target_seq[0])
- 
+        outputs.append(output[0].detach().numpy())
+    if epoch == n_epochs:
+        #plot x(t) for every ten runs and compare to the target sequence
+        t=target_seq[0].detach().numpy()
+        tx=[x[0] for x in t]
+        plt.scatter(range(9), tx, label = 'target')
+        for i in range(len(outputs)):
+            rgb = (random.random(), random.random(), random.random())
+            ox=[x[0] for x in outputs[i]]
+            plt.scatter(range(9), ox, c=[rgb], label = str(i))
+            leg=plt.legend()
+        plt.show()
+        
 
  
